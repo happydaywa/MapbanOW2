592091438ce0ccc008a998fe214bc353bd49069d
happydaywa <micah.pope2010@gmail.com>
2025-10-25 12:56:50 +1000

    lists through winrates for all maps

--- MapBan.py
+++ MapBan.py
@@ -1,1 +1,20 @@
-print("hello")
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://overwatch.blizzard.com/en-us/rates/?input=PC&map=all-maps&region=Americas&role=All&rq=1&tier=All'
+source = requests.get(url).text
+
+soup = BeautifulSoup(source, 'lxml')
+
+winrates = {}
+
+for span in soup.find_all(class_="percent-value"):
+    hero_id = span.get("id")
+    if hero_id and hero_id.endswith("-winrate-value"):
+        hero_name = hero_id.replace("-winrate-value", "")
+        if span.text == '--':
+            continue
+        percent = float(span.text.strip('%'))
+        winrates[hero_name] = percent
+
+print(winrates)
